2b0d8a7f065b0239833a164b341b97504168faf8
ruslstepanson753 <ruslstepanson753@gmail.com>
2025-08-26 11:54:28 +0300

    spring config hiber remove jdbc

--- src/main/java/ru/alishev/springcourse/dao/BookDAO.java
+++ src/main/java/ru/alishev/springcourse/dao/BookDAO.java
@@ -12,40 +12,27 @@ import java.util.List;
 @Component
 public class BookDAO {
 
-    private final JdbcTemplate jdbcTemplate;
 
-    @Autowired
-    public BookDAO(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+
+    public BookDAO() {
+
     }
 
     public List<Book> getBooks() {
-        List<Book> books = jdbcTemplate.query("select * from book", new BeanPropertyRowMapper<>(Book.class));
-        return books;
+        return null;
     }
 
     public Book getBook(int id) {
-        return jdbcTemplate.queryForObject("select * from book where book_id = ?", new Object[] { id }, new BeanPropertyRowMapper<Book>(Book.class));
+        return null;
     }
 
     public void save(Book book) {
-        jdbcTemplate.update("insert into book(book_name,author,year_book) values (?,?,?)",
-                book.getBookName(), book.getAuthor(), book.getYearBook());
     }
 
     public void update(int id, Book book) {
-        jdbcTemplate.update("UPDATE book SET book_name = ?, author = ?, year_book = ? WHERE book_id = ?",
-                book.getBookName(), book.getAuthor(), book.getYearBook(), id);
     }
 
     public List<Book> getBooksByPerson(Person person) {
-        Integer id = person.getPersonId();
-        List<Book> books = jdbcTemplate.query("" +
-                        "SELECT * FROM book " +
-                        "JOIN person ON person.person_id = book.person_id " +
-                        "WHERE book.person_id = ?", // Указываем конкретную таблицу
-                new Object[] { id },
-                new BeanPropertyRowMapper<Book>(Book.class));
-        return books;
+        return null;
     }
 }
